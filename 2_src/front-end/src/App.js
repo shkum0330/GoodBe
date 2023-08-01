ee4d88a81e90ebc3ab5f38af8519be73d376d1ce
seongwonlee <jasmine1714@naver.com>
2023-08-01 17:19:03 +0900

    Seongwon made Edupage

--- 2_src/front-end/src/App.js
+++ 2_src/front-end/src/App.js
@@ -4,14 +4,23 @@ import AccountHeader from './components/common/AccountHeader';
 import EduHeader from './components/common/EduHeader';
 import AccountFindHeader from  './components/common/AccountFindHeader';
 import Footer from './components/common/Footer';
+import Navbar from './components/common/Navbar';
 
+import InputForm from './components//PasswordFind/InputForm';
+import EduHead from './components//EduDetail/EduHead';
+
+import EduTab from './components/EduDetail/EduTab';
+import EduDetailContent from './components/EduDetail/EduDetailContent';
+import CurriculumContent from './components/Curriculum/CurriculumContent';
+import EduReviewContent from './components/EduReview/EduReviewContent';
 
 function App() {
   return (
     <div className="App">
-      <AccountFindHeader />
-      <EduHeader />
-      <AccountHeader />
+      <Navbar />
+      <EduHead />
+      <EduTab />
+      <EduReviewContent />
     </div>
   );
 }
