634af70a12a2b653fb62ebceff2e51f3a7e7906b
유지나 <wlskb@naver.com>
2023-08-01 15:43:50 +0900

    [+] add BoardDetailContent

--- 2_src/front-end/src/App.js
+++ 2_src/front-end/src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import AccountHeader from './components/common/AccountHeader';
 import EduHeader from './components/common/EduHeader';
 import AccountFindHeader from  './components/common/AccountFindHeader';
+import BoardDetailContent from './components/BoardDetail/BoardDetailContent';
+import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
+import styled from 'styled-components';
 
 
+const Container=styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 function App() {
   return (
-    <div className="App">
-      <AccountFindHeader />
-      <EduHeader />
-      <AccountHeader />
-    </div>
+    <Container>
+      <Navbar/>
+        <BoardDetailContent/>
+      <Footer/>
+    </Container>
   );
 }
 
