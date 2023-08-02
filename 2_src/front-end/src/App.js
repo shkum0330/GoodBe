386e6279cce2d557236b286dc079a85749e1b6cc
Jungseok <jungsuk7083@gmail.com>
2023-08-02 17:45:50 +0900

    Jungseok made JobDetail Almost!

--- 2_src/front-end/src/App.js
+++ 2_src/front-end/src/App.js
@@ -1,17 +1,22 @@
 
-import './App.css';
-import AccountHeader from './components/common/AccountHeader';
-import EduHeader from './components/common/EduHeader';
-import AccountFindHeader from  './components/common/AccountFindHeader';
+// import './App.css';
+// import AccountHeader from './components/common/AccountHeader';
+// import EduHeader from './components/common/EduHeader';
+// import AccountFindHeader from  './components/common/AccountFindHeader';
 import Footer from './components/common/Footer';
-
+import Navbar from './components/common/Navbar';
+import JobHeader from './components/JobDetail/JobHeader';
+import JobDetailContent from './components/JobDetail/JobDetailContent';
+import EduRecommendRelatedJob from './components/JobDetail/EduRecommendRelatedJob';
 
 function App() {
   return (
     <div className="App">
-      <AccountFindHeader />
-      <EduHeader />
-      <AccountHeader />
+      <Navbar/>
+      <JobHeader/>
+      <JobDetailContent/>
+      <EduRecommendRelatedJob/>
+      <Footer/>
     </div>
   );
 }
