9f342b1b861507a6a77d53854f84a3ee9c56300a
유지나 <wlskb@naver.com>
2023-08-03 17:57:58 +0900

    [+] add board main

--- 2_src/front-end/src/App.js
+++ 2_src/front-end/src/App.js
@@ -1,25 +1,31 @@
 
 import './App.css';
-import AccountHeader from './components/common/AccountHeader';
-import EduHeader from './components/common/EduHeader';
-import AccountFindHeader from  './components/common/AccountFindHeader';
-import BoardDetailContent from './components/BoardDetail/BoardDetailContent';
+
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import styled from 'styled-components';
+import BoardList from './components/BoardMain/BoardList';
+
 
 
 const Container=styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: cetner;
+  align-items:center;
 `
+
+
 function App() {
   return (
     <Container>
+
+    
       <Navbar/>
-        <BoardDetailContent/>
+      <BoardList/>
+      <br></br>
       <Footer/>
+
     </Container>
   );
 }
