2bde99abdfb9d63c1228251c5590e06f2123b0fd
Lee Seunghwan <tmdghks304@gmail.com>
2023-08-03 15:51:17 +0900

    feat : 데이터 수집 및 db에 저장하는 기능 추가

--- 2_src/back-end/data/job_data_crawling.py
+++ 2_src/back-end/data/job_data_crawling.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from collections import deque
+import pymysql
 
 # 데이터 저장할 리스트
 auth_no_list = deque()
@@ -26,6 +27,7 @@ with open(job_api_path, "r") as f:
 # 채용공고 아이디 리스트 받아오기
 for ocp in occupation:
     for page in range(1,1000):
+        print(f"page {page} crawling...")
         api_url = f"http://openapi.work.go.kr/opi/opi/opia/wantedApi.do?authKey={api_key}&callTp=L&returnType=XML&startPage={page}&display=100&occupation={ocp}"
         cnt = 0
         req = requests.get(api_url)
@@ -119,4 +121,34 @@ for wanted_auth_no in auth_no_list:
     job_df.loc[idx] = job_data_list
 
 # csv로 저장
-job_df.to_json("job_data.json")
+job_df.to_json("job_data2.json")
+
+
+# db connection
+conn = pymysql.connect(host = "localhost",port=3306,
+                       user = "root",
+                       password = "ssafy",
+                       database = "test",
+                       charset = "utf8")
+
+curs = conn.cursor()
+
+# db insert
+for i in range(len(job_df)):
+    id = job_df.id.loc[i]
+    company_id = job_df.company_id.loc[i]
+    company_data = job_df.company_data.loc[i]
+    company_url = job_df.company_url.loc[i]
+    job_content = job_df.job_content.loc[i]
+    end_date = job_df.end_date.loc[i]
+    sal = job_df.sal.loc[i]
+    job_data = job_df.job_data.loc[i]
+
+    sql2 = f"insert into job_post values('{id}','{company_id}','{company_data}','{company_url}','{job_content}','{end_date}','{sal}','{job_data}')"
+
+    try:
+        curs.execute(sql2)
+    except:
+        pass
+
+conn.commit()
