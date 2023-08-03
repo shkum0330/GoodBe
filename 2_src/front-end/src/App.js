33cfb7ce2b856e366d99b1be2e3e1db92af4b464
seongwonlee <jasmine1714@naver.com>
2023-08-03 15:36:37 +0900

    Jasmine

--- 2_src/front-end/src/App.js
+++ 2_src/front-end/src/App.js
@@ -1,19 +1,26 @@
 
 import './App.css';
 import Navbar from './components/common/Navbar';
+import Sidebar from './components/common/Sidebar';
 import Footer from './components/common/Footer';
 import JobList from './components/JobMain/JobList';
 import JobTab from './components/JobMain/JobTab';
 import EduList from './components/EduList/EduList';
 import SearchJob from './components/JobMain/SearchJob';
 import EduSearchHeader from './components/EduList/EduSearchHeader'
+import MyLikeEduHeader from './components/MyEduLike/MyLikeEduHeader';
+import MyLikeEduList from './components/MyEduLike/MyLikeEduList'
 
 function App() {
   return (
    <div>
-    {/* <JobList /> */}
-    <EduSearchHeader />
-    <EduList />
+    <Sidebar />
+    <Navbar />
+    <MyLikeEduHeader />
+    <MyLikeEduList />
+
+
+    {/* <EduList /> */}
    </div>
   );
 }
