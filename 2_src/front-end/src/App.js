62ff6bd40174dfa7ba55f0ac177eb30cc8781b30
seongwonlee <jasmine1714@naver.com>
2023-08-07 10:52:59 +0900

    app 지움

--- 2_src/front-end/src/App.js
+++ 2_src/front-end/src/App.js
@@ -1,44 +1,19 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
+import React from 'react';
 import Navbar from './components/common/Navbar';
 import Sidebar from './components/common/Sidebar';
 import Footer from './components/common/Footer';
 import JobList from './components/JobMain/JobList';
-import JobTab from './components/JobMain/JobTab';
-import EduList from './components/EduList/EduList';
-import SearchJob from './components/JobMain/SearchJob';
-import EduSearchHeader from './components/EduList/EduSearchHeader';
-import MypageHome from './components/MyPageHome/MypageHome';
-import MyPageHomeHeader from './components/MyPageHome/MyPageHomeHeader';
-import MyPageInformationHeader from './components/MyInfoChange/MyPageInformationHeader';
-  
-
-import React from 'react'
+import MyConsultingReservation from './components/MyConsulting/MyConsultingReservation'
+import MyEduConsultingHeader from './components/MyConsulting/MyEduConsultingHeader'
 
 export default function App() {
   return (
     <div>
-       <Sidebar/>
-      <Navbar/>
-      <JobList/>
-      {/* <MyPageInformationHeader/> */}
-      {/* <MyPageHomeHeader/> */}
-      {/* <MypageHome/> */}
-      <Footer/>
-
+      <Sidebar />
+      <Navbar />
+      <MyEduConsultingHeader />
+      <MyConsultingReservation />
+      {/* <Footer /> */}
     </div>
-
-
   );
-
-<<<<<<< HEAD
- 
 }
-=======
-export default App;
->>>>>>> d8dd12d47ef8fa46aae9291d340248841a491767
->>>>>>> 888273136400abfe461823a309f5a684920decd4
->>>>>>> 2faf565c8cf82c0722ecd13bc36e7b209f8c578b
