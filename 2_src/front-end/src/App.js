61a15304fbfd8105b7de2ecf9659d628c7144170
Jungseok <jungsuk7083@gmail.com>
2023-08-08 14:18:28 +0900

    Jungseok Main page

--- 2_src/front-end/src/App.js
+++ 2_src/front-end/src/App.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 // import Navbar from './components/common/Navbar';
 // import Sidebar from './components/common/Sidebar';
@@ -20,37 +18,14 @@
 import React from 'react'
 import GoLogin from './components/SignUp/GoLogin';
 import AboutAccount from './components/Login/AboutAccount';
-export default function App() {
-  return (
-    <div>
-      <AboutAccount/>
-=======
-import React from 'react';
-import Navbar from './components/common/Navbar';
-import Sidebar from './components/common/Sidebar';
-import Footer from './components/common/Footer';
-import JobList from './components/JobMain/JobList';
-import MyConsultingReservation from './components/MyConsulting/MyConsultingReservation'
-import MyEduConsultingHeader from './components/MyConsulting/MyEduConsultingHeader'
-import MyBoardHeader from './components/MyBoard/MyBoardHeader'
-import MyBoardContainer from './components/MyBoard/MyBoardContainer'
-import Advertise from './components/Main/Advertise'
-import SearchMain from './components/Main/SearchMain'
-import EduExampleRecommend from './components/Main/EduExampleRecommend'
+import EnterpriseIntroduction from './components/Main/EnterpriseIntroduction';
+import EduInterest from './components/Main/EduInterset';
 
 export default function App() {
   return (
     <div>
-      <Navbar />
-      <Advertise />
-      <SearchMain />
-      <EduExampleRecommend />
-      {/* <Footer /> */}
->>>>>>> 33baf1a2099ff979d6f98220f3cd3d7fbee7511e
+      <EnterpriseIntroduction/>
+      <EduInterest/>
     </div>
   );
-<<<<<<< HEAD
 };
-=======
-  }
->>>>>>> 33baf1a2099ff979d6f98220f3cd3d7fbee7511e
