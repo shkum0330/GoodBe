cc89f6b2ae6e2d934c17da57cc51d95a940450d7
Lee Seunghwan <tmdghks304@gmail.com>
2023-08-09 09:30:22 +0900

    feat : 키워드 추출 기능 추가 검색어를 입력하면 검색어에 맞는 채용공고 정보를 불러오고 그 정보에서 연관성이 높은 상위 20개 키워드를 추출함

--- 2_src/back-end/analysis/api/views.py
+++ 2_src/back-end/analysis/api/views.py
@@ -4,8 +4,9 @@ from .serializers import job_post_serializer
 from .models import job_post
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from nlp import nlp
+from nlp import keyword_abstraction as ka
 
 @api_view(['GET'])
 def get_keyword_abstraction(request, keyword):
-    return Response(keyword)
+    ab_words = ka.keyword_abstraction(keyword)
+    return Response(ab_words)
