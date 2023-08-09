288f9f26551885685e977dc75d55a2517f58bafd
seongwonlee <jasmine1714@naver.com>
2023-08-09 14:12:51 +0900

    d

---
+++ 2_src/front-end/src/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import BoardLisB from '../components/BoardMain/BoardList'
+
+
+
+export default function App() {
+  return (
+    <div>
+        <BoardLisB/>
+    </div>
+  );
+};
