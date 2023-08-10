7237b8cafe2db00d48eb662ea0c2df4d30eadeba
seongwonlee <jasmine1714@naver.com>
2023-08-10 09:34:03 +0900

    d

--- 2_src/front-end/src/pages/CamChat.jsx
+++ 2_src/front-end/src/pages/CamChat.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import VideoButtons from '../components/CamChat/VideoButtons'
 import VideoCam from '../components/CamChat/VideoCam'
@@ -13,16 +13,3 @@ export default function CamChat() {
     </div>
   )
 }
-=======
-import React from 'react';
-
-const CamChat = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default CamChat;
->>>>>>> 071d3bc75c51ad0df3bf58e7dd742c9dca6e2319
