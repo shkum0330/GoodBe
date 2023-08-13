bcccfea1b3c975f7b2f4680634e0f5be988e5d70
seongwonlee <jasmine1714@naver.com>
2023-08-13 14:24:08 +0900

    Jasmine 로그인 모달창, 네브바 디자인 수정

--- 2_src/front-end/src/pages/SignUp.jsx
+++ 2_src/front-end/src/pages/SignUp.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import GoLogin from '../components/SignUp/GoLogin';
+
 
 export default function SignUp() {
   return (
     <div>
-        <GoLogin/>
-        
+
 
     </div>
   )
