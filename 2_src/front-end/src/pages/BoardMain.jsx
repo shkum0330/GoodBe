49e6dbcfccf2679111c769f25c2599d1ea546b60
Jungseok <jungsuk7083@gmail.com>
2023-08-17 10:28:42 +0900

    게시판

--- 2_src/front-end/src/pages/BoardMain.jsx
+++ 2_src/front-end/src/pages/BoardMain.jsx
@@ -5,31 +5,6 @@ import Footer from '../components/common/Footer'
 import Loading from '../components/Loading/Loading';
 
 export default function BoardMain() {
-  const [loading, setLoading] = useState(true);
-
-const mainApi = async () => {
-	setLoading(true); // api 호출 전에 true로 변경하여 로딩화면 띄우기
-    try {
-      const response = await fetch(`api url`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(),
-      });
-
-      const result = await response.json();
-      console.log('mainData', result);
-      setLoading(false); // api 호출 완료 됐을 때 false로 변경하려 로딩화면 숨김처리
-    } catch (error) {
-      window.alert(error);
-    }
-};
-
-    useEffect(() => {
-        mainApi();
-    }, []);
 
   return (
     <div>
